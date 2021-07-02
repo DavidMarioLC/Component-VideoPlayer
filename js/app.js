8c5f1f2a54d70d16c31952120f57521b5c859f0b
Jarrison Cano <jarrison.cano@hotmail.com>
2021-07-01 19:36:47 -0500

    :zap: refactor: restructure app js & improve styles

--- js/app.js
+++ js/app.js
@@ -1,52 +1,50 @@
-const video = document.querySelector('#video');
-const buttonPlay = document.querySelector('#play');
-const buttonPause = document.querySelector('#pause');
-const backward = document.querySelector('#backward');
-const forward = document.querySelector('#forward');
-const progress = document.querySelector('#range');
- 
-function playingVideo(){
-    video.play();
-    buttonPlay.hidden = true;
-    buttonPause.hidden = false;
+const video = document.querySelector('#video')
+const buttonPlay = document.querySelector('#play')
+const buttonPause = document.querySelector('#pause')
+const backward = document.querySelector('#backward')
+const forward = document.querySelector('#forward')
+const progress = document.querySelector('#range')
+
+function playingVideo() {
+  video.play()
+  buttonPlay.hidden = true
+  buttonPause.hidden = false
 }
 
-function stoppingVideo(){
-    video.pause();
-    buttonPause.hidden = true;
-    buttonPlay.hidden = false;
+function stoppingVideo() {
+  video.pause()
+  buttonPause.hidden = true
+  buttonPlay.hidden = false
 }
 
-function backwardVideo(){
-// video.currentTime no lo encontre
-console.log(video.currentTime)
-video.currentTime -=10
+function backwardVideo() {
+  video.currentTime -= 10
 }
 
-function forwardVideo(){
-    console.log(video.currentTime)
-    video.currentTime +=10
+function forwardVideo() {
+  video.currentTime += 10
 }
 
-
-function loadedVideo(){
-    this.max = this.duration;
+function loadedVideo() {
+  this.max = this.duration
 }
 
-function updateVideo(){
-    progress.value = this.currentTime
-    
-
+function updateVideo() {
+  progress.value = this.currentTime
 }
 
-function moveProgress(){
-    video.currentTime =this.value
+function moveProgress() {
+  video.currentTime = this.value
 }
 
-buttonPlay.addEventListener('click',playingVideo);
-buttonPause.addEventListener('click',stoppingVideo);
-backward.addEventListener('click',backwardVideo);
-forward.addEventListener('click',forwardVideo);
-video.addEventListener('loadedmetadata',loadedVideo)
-video.addEventListener('timeupdate',updateVideo)
-progress.addEventListener('input',moveProgress)
+function addListener(target, callback, event = 'click') {
+  target.addEventListener(event, callback)
+}
+
+addListener(buttonPlay, playingVideo)
+addListener(buttonPause, stoppingVideo)
+addListener(backward, backwardVideo)
+addListener(forward, forwardVideo)
+addListener(video, loadedVideo, 'loadedmetadata')
+addListener(video, updateVideo, 'timeupdate')
+addListener(progress, moveProgress, 'input')
